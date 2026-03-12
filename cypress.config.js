95f35c58f3be35b6571fe473a52bd9d856572d98
Mateo <mateobinance1@gmail.com>
2026-03-11 22:12:51 -0300

    fix issue on test CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: true,
   video: true,
-  retries: 1,
+  retries: 2,
   e2e: {
     specPattern: "cypress/src/tests/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
     baseUrl: "https://practicesoftwaretesting.com",
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36"
   }
 
 });
